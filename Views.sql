e3a7b6150cfab0fa3e503c39f2970c65b25ab7cc
mossjere <55941085+mossjere@users.noreply.github.com>
2019-11-12 15:33:18 -0700

    Update Views.sql

--- Views.sql
+++ Views.sql
@@ -24,48 +24,61 @@ CREATE VIEW RSE_Active_Computers AS
 
 
 CREATE VIEW RSE_Current_Employees AS
-    SELECT 
-            E.FirstName + ' ' + E.LastName AS "Employee",
-            S.FirstName + ' ' + S.LastName AS "Current Supervisor",
-            EJ.Salary,
-            EJ.Title,
-            CASE
-                WHEN EJ.JobStart = E.Hired
-                    THEN 'NONE'
-                ELSE CONVERT(varchar(10),EJ.JobStart,20)
-            END AS "LAST INCREASE",
-            
-            CASE 
-                WHEN EJ.JobStart = E.Hired
-                THEN 'NONE'
-                ELSE
-                    CONVERT(varchar(2),
-                            (Select (B.Salary / C.Salary * 100)- 100
-                                FROM EmployeeJobs B 
-                                JOIN 
-                                (
-                                    SELECT A.EmployeeJobKey, A.EmployeeKey, A.Salary--, D.FirstName, D.LastName
-                                    FROM dbo.EmployeeJobs A JOIN dbo.Employees D ON(A.EmployeeKey = D.EmployeeKey)
-                                    RIGHT JOIN dbo.EmployeeJobs C ON(A.EmployeeJobKey = C.EmployeeJobKey)
-                                    WHERE A.Salary < 
-                                    (
-                                        SELECT max(B.salary) 
-                                        FROM dbo.EmployeeJobs B 
-                                        Where A.EmployeeKey = B.EmployeeKey
-                                    )
-                                GROUP BY A.EmployeeJobKey, A.EmployeeKey, A.Salary
-                            ) AS C ON (C.EmployeeKey = B.EmployeeKey)
-                            WHERE 
-                                B.JobFinish is null),
-                    20) + '%'
-            END AS "Percentage increase"
+  	SELECT 
+		E.FirstName + ' ' + E.LastName AS "Employee",
+		S.FirstName + ' ' + S.LastName AS "Current Supervisor",
+		EJ.Salary,
+		EJ.Title,
+		CASE
+			WHEN EJ.JobStart = E.Hired
+				THEN 'NONE'
+			ELSE CONVERT(varchar(10),EJ.JobStart,20)
+		END AS "LAST INCREASE",
+		
+		CASE 
+			WHEN C.EmployeeJobKey is NULL
+			THEN 'NONE'
+			ELSE
+				(CONVERT(varchar(10),(B.Salary / C.Salary * 100)- 100)) + '%'
+					
+		END AS "Percentage increase"
 
-        FROM
-            dbo.Employees E
-            JOIN
-            dbo.Employees S
-            ON S.CurrentSupervisorEmployeeKey = E.EmployeeKey
-            JOIN
-            dbo.EmployeeJobs EJ
-            ON EJ.EmployeeKey = E.EmployeeKey
-        WHERE EJ.JobFinish is NULL
+	FROM
+		dbo.Employees E
+		LEFT JOIN
+		dbo.Employees S
+		ON S.CurrentSupervisorEmployeeKey = E.EmployeeKey
+		LEFT JOIN
+		dbo.EmployeeJobs EJ
+		ON EJ.EmployeeKey = E.EmployeeKey
+		LEFT JOIN
+		EmployeeJobs B
+		ON(B.EmployeeKey = E.EmployeeKey)
+		LEFT JOIN 
+		(
+			SELECT 
+				A.EmployeeJobKey, 
+				A.EmployeeKey, 
+				A.Salary--, D.FirstName, D.LastName
+			FROM 
+				dbo.EmployeeJobs A 
+				JOIN 
+				dbo.Employees D 
+				ON(A.EmployeeKey = D.EmployeeKey)
+				RIGHT JOIN 
+				dbo.EmployeeJobs C 
+				ON(A.EmployeeJobKey = C.EmployeeJobKey)
+			WHERE A.Salary < 
+			(
+				SELECT 
+					max(B.salary) 
+				FROM 
+					dbo.EmployeeJobs B 
+				Where 
+					A.EmployeeKey = B.EmployeeKey
+			)
+			GROUP BY A.EmployeeJobKey, A.EmployeeKey, A.Salary
+		) AS C 
+		ON (C.EmployeeKey = B.EmployeeKey)
+	WHERE EJ.JobFinish is NULL
+	    AND B.JobFinish IS NULL
