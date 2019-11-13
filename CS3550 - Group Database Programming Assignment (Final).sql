5dc20f64b3fc480df8ce3caeddb9cba5ea64d15a
Joshua <64.josh@gmail.com>
2019-11-12 17:19:49 -0700

    Added views to main file

--- CS3550 - Group Database Programming Assignment (Final).sql
+++ CS3550 - Group Database Programming Assignment (Final).sql
@@ -968,7 +968,7 @@ CREATE OR ALTER PROCEDURE dbo.RSE_SP_CreateComputer
 */
 
 GO -- This function returns the value of the computer
-CREATE FUNCTION RSE_CalcCompValue(
+CREATE OR ALTER FUNCTION RSE_CalcCompValue(
 	@purchaseDate date,
 	@purchaseCost money
 )
@@ -978,11 +978,10 @@ BEGIN
 	DECLARE @ratio float = CAST(@monthsBetween AS float) / CAST(36 AS float)
 	RETURN CAST((@purchaseCost - (@ratio * @purchaseCost)) AS money)
 END
---drop function dbo.RSE_CalcCompValue
 --SELECT dbo.RSE_CalcCompValue('2018-04-16', 1500) -- TEST: Should return 1500 - (18/36)(1500) = 750
 
 GO -- This function returns the average salary for a given employee level
-CREATE FUNCTION RSE_CalcAvgSalary(
+CREATE OR ALTER FUNCTION RSE_CalcAvgSalary(
 	@employeeLevelKey int
 )
 RETURNS money AS
@@ -1005,7 +1004,6 @@ BEGIN
 	)
 	RETURN CAST((@empSalaryTotal / @empCount) AS money)
 END
---drop function dbo.RSE_CalcAvgSalary
 
 /*
  - Views that need to be written
@@ -1021,7 +1019,89 @@ END
 		recieved their last increase (if any), and the percentage increase 
 		that raise was
 */
+GO
+CREATE VIEW RSE_Active_Computers AS
+	SELECT 
+		C.ComputerKey, 
+		E.LastName,
+		E.FirstName,
+		C.PurchaseDate,
+		C.ComputerDetails, 
+		dbo.RSE_CalcCompValue(C.PurchaseDate, C.PurchaseCost) AS "Current Worth"
+	FROM
+		dbo.Computers C
+		LEFT JOIN
+		dbo.EmployeeComputers EC
+		ON C.ComputerKey = EC.ComputerKey
+		LEFT JOIN
+		dbo.Employees E
+		ON EC.EmployeeKey = E.EmployeeKey
+		JOIN
+		dbo.ComputerStatuses CS
+		ON C.ComputerStatusKey = CS.ComputerStatusKey
+		WHERE CS.ActiveStatus = 1;
 
+GO
+CREATE VIEW RSE_Current_Employees AS
+  	SELECT 
+		E.FirstName + ' ' + E.LastName AS "Employee",
+		S.FirstName + ' ' + S.LastName AS "Current Supervisor",
+		EJ.Salary,
+		EJ.Title,
+		CASE
+			WHEN EJ.JobStart = E.Hired
+				THEN 'NONE'
+			ELSE CONVERT(varchar(10),EJ.JobStart,20)
+		END AS "LAST INCREASE",
+		
+		CASE 
+			WHEN C.EmployeeJobKey is NULL
+			THEN 'NONE'
+			ELSE
+				(CONVERT(varchar(10),(B.Salary / C.Salary * 100)- 100)) + '%'
+					
+		END AS "Percentage increase"
+
+	FROM
+		dbo.Employees E
+		LEFT JOIN
+		dbo.Employees S
+		ON S.CurrentSupervisorEmployeeKey = E.EmployeeKey
+		LEFT JOIN
+		dbo.EmployeeJobs EJ
+		ON EJ.EmployeeKey = E.EmployeeKey
+		LEFT JOIN
+		EmployeeJobs B
+		ON(B.EmployeeKey = E.EmployeeKey)
+		LEFT JOIN 
+		(
+			SELECT 
+				A.EmployeeJobKey, 
+				A.EmployeeKey, 
+				A.Salary--, D.FirstName, D.LastName
+			FROM 
+				dbo.EmployeeJobs A 
+				JOIN 
+				dbo.Employees D 
+				ON(A.EmployeeKey = D.EmployeeKey)
+				RIGHT JOIN 
+				dbo.EmployeeJobs C 
+				ON(A.EmployeeJobKey = C.EmployeeJobKey)
+			WHERE A.Salary < 
+			(
+				SELECT 
+					max(B.salary) 
+				FROM 
+					dbo.EmployeeJobs B 
+				Where 
+					A.EmployeeKey = B.EmployeeKey
+			)
+			GROUP BY A.EmployeeJobKey, A.EmployeeKey, A.Salary
+		) AS C 
+		ON (C.EmployeeKey = B.EmployeeKey)
+	WHERE EJ.JobFinish is NULL
+	    AND B.JobFinish IS NULL
+GO
 /*
  - Triggers that need to be written
 
